12959e30611bab4f30e280dcd79f347f9795b89b
j.luna <j.luna@tesipro.com>
2019-11-27 15:32:44 +0100

    Dropdown final

--- src/app/shared/ae-dropdown/ae-dropdown.directive.ts
+++ src/app/shared/ae-dropdown/ae-dropdown.directive.ts
@@ -5,31 +5,27 @@ import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/cor
 })
 export class AeDropdownDirective implements OnInit {
   @Input() public placement: string = 'bottom';
-  @Input() public delay: string = '0';
 
   private button: HTMLElement;
   private content: HTMLElement;
-  private mouseOnDropdown = false;
 
   constructor(private el: ElementRef) {
   }
 
   @HostListener('document:click', ['$event.target']) onMouseEnter(targetElement) {
-    const buttonClicked = this.button.isEqualNode(targetElement);
+    const buttonClicked = this.button.isEqualNode(targetElement) || this.button.contains(targetElement);
     buttonClicked ? this.show() : this.hide();
   }
 
   @HostListener('window:scroll') onScroll() {
-    this.mouseOnDropdown = false;
-    if (this.content) {
-      this.hide();
-    }
+    this.hide();
   }
 
   public ngOnInit() {
     this.el.nativeElement.classList.add('dropdown');
     this.button = this.el.nativeElement.children[0];
     this.content = this.el.nativeElement.children[1];
+    this.content.style['top'] = this.button.offsetHeight + 5 + 'px';
     this.hide();
   }
 
