b3503bd8546673a0e5477c012da293d26f912d14
jalu <javidevictoria+gitkraken@gmail.com>
2019-12-06 16:01:00 +0100

    Template de create member

--- src/app/base/members/modals/create-member.modal.ts
+++ src/app/base/members/modals/create-member.modal.ts
@@ -1,46 +1,44 @@
-import { Component }     from '@angular/core';
-import { ModalTemplate } from '../../../core/template/modal-template';
+import { Component, Input } from '@angular/core';
 
 @Component({
   template: `
-    <ng-template #template let-entity>
-      <div class="w-full px-2 mt-2">
-        <div class="flex flex-wrap -mx-2">
-          <div class="w-full md:w-1/2 lg:w-1/3 px-2">
-            <ae-input [model]="entity"
-                      key="givenName"
-                      [required]="true"
-                      minLength="5"
-                      [placeholder]="'Jose pepe'">
-            </ae-input>
-          </div>
-          <div class="w-full md:w-1/2 lg:w-1/3 px-2">
-            <ae-input [model]="entity"
-                      key="middleName"
-                      [required]="true"
-                      minLength="5">
-            </ae-input>
-          </div>
-          <div class="w-full md:w-1/2 lg:w-1/3 px-2">
-            <ae-input [model]="entity"
-                      key="surName"
-                      [required]="true"
-                      minLength="5">
-            </ae-input>
-          </div>
-
-          <div class="w-full px-2">
-            <ae-select [model]="entity"
-                       key="allergyList"
-                       type="tag"
-                       [multiple]="true"
-                       [closeOnSelect]="false">
-            </ae-select>
-          </div>
+    <div class="w-full px-2 mt-2">
+      <div class="flex flex-wrap -mx-2">
+        <div class="w-full md:w-1/2 lg:w-1/3 px-2">
+          <ae-input [model]="entity"
+                    key="givenName"
+                    [required]="true"
+                    minLength="5"
+                    [placeholder]="'Jose pepe'">
+          </ae-input>
         </div>
-        <pre class="w-full">{{entity | json}}</pre>
+        <div class="w-full md:w-1/2 lg:w-1/3 px-2">
+          <ae-input [model]="entity"
+                    key="middleName"
+                    [required]="true"
+                    minLength="5">
+          </ae-input>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 px-2">
+          <ae-input [model]="entity"
+                    key="surName"
+                    [required]="true"
+                    minLength="5">
+          </ae-input>
+        </div>
+
+        <div class="w-full px-2">
+          <ae-select [model]="entity"
+                     key="allergyList"
+                     type="tag"
+                     [multiple]="true"
+                     [closeOnSelect]="false">
+          </ae-select>
+        </div>
       </div>
-    </ng-template>`,
+      <pre class="w-full">{{entity | json}}</pre>
+    </div>`,
 })
-export class CreateMemberModal extends ModalTemplate {
+export class CreateMemberModal {
+  @Input() public entity;
 }
